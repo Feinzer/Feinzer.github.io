d39ceffe5674910ec0383d4a0ef4f2ceaa1c4dfa
Feinzer <danhernan23i@gmail.com>
2020-12-12 14:49:56 -0400

    Fix card centering

--- layouts/default.vue
+++ layouts/default.vue
@@ -5,10 +5,10 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="min-h-screen flex flex-col">
     <Circles class="z-10" />
     <Diamonds class="z-20" />
     <Navigation class="relative z-40" /> 
-    <Nuxt class="relative z-30" />
+    <Nuxt class="z-30" />
   </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -21,12 +21,12 @@ export default {
 </script>
 
 <template>
-  <div class="h-full p-8 w-full flex flex-col items-center justify-center">
-    <div class="sm:w-1/2 flex flex-col justify-center items-center">
+  <div class="absolute h-full w-full p-8 flex flex-col items-center justify-center">
+    <div class="sm:w-3/4 flex flex-col justify-center items-center">
       <Card :type="CardType.IMAGE" />
       <Card :type="CardType.TITLE" content="Daniel Hernández" />
     </div>
-    <div class="w-4/5 md:w-1/2 flex flex-col justify-center items-center">
+    <div class="w-4/5 md:w-1/2 lg:w-1/3 xl:w-1/4 flex flex-col justify-center items-center">
       <Card
         :type="CardType.INFO"
         content="Electronics engineering student and self taught developer with experience on Web, Mobile Apps, Games and Linux kernel development."
