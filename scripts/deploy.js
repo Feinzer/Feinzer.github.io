7a737c835b4e6d001821fad64dce89b24e07ce77
Feinzer <danhernan23i@gmail.com>
2020-12-16 11:04:40 -0400

    Make a proper deployment script

---
+++ scripts/deploy.js
@@ -0,0 +1,31 @@
+const { execSync } = require('child_process')
+const fs = require('fs-extra')
+require('dotenv').config()
+
+const deployFolder = '.deploy'
+const buildFolder = 'dist'
+const cwd = process.cwd()
+
+if (!fs.pathExistsSync(deployFolder)) {
+  console.log('E: Local deployment repository not found')
+  console.log(`Cloning from ${process.env.GIT_REMOTE} into ${deployFolder}...\n`)
+  execSync(`git clone -b master ${process.env.GIT_REMOTE} ${deployFolder}`, { cwd })
+}
+
+if (!fs.pathExistsSync(buildFolder)) throw 'Build directory not found'
+
+console.log('Updating local deployment directory...\n')
+fs.copySync(buildFolder, deployFolder, { overwrite: true })
+
+console.log('Committing automated deployment...\n')
+execSync([
+  `cd ${deployFolder}`,
+  'git add .',
+  'timestamp=$(date \"+%m-%d-%Y %T\")',
+  'git commit --allow-empty -m \"Automated deployment $timestamp\";'
+].join('&&'), { cwd });
+
+console.log('Pushing...\n');
+execSync(`cd ${deployFolder} && git push ${process.env.GIT_REMOTE} master`, { cwd });
+
+console.log('\nDeployment successful!\n')
