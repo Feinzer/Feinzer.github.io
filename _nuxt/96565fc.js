669f28ef6c2ea8f97e423b41cf9976d0ef2b6141
Feinzer <danhernan23i@gmail.com>
2020-12-23 00:10:14 -0400

    Automated deployment 2020-12-23 04:10:14 UTC

---
+++ _nuxt/96565fc.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{244:function(n,t,e){"use strict";e.r(t);var o={name:"About",transition:"load",head:function(){return{title:"About - Feinzer"}}},r=e(7),component=Object(r.a)(o,(function(){var n=this.$createElement;return(this._self._c||n)("div",[this._v("\n  aaaaaaaaaaaa\n")])}),[],!1,null,null,null);t.default=component.exports}}]);
